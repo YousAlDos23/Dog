a4cb14728718fd3ddfff55448123c26149b50099
Josep <Josep@Youssef-msi>
2021-02-28 09:08:30 +0300

    Some bug fixes

--- Dog/src/Dog.java
+++ Dog/src/Dog.java
@@ -1,12 +1,10 @@
 
 public class Dog {
+	public static void maim(String[] args) {
+		
+	}
 	
-	String breed;
-	String size;
-	String colour;
-	int age;
-	
-	void Doginfo() {
+	public static void Doginfo(String breed , String size , String colour , int age) {
 		System.out.println("Breed is " + breed);
 		System.out.println("Size is " + size);
 		System.out.println("Colour is " + colour);
--- Dog/src/TestDog.java
+++ Dog/src/TestDog.java
@@ -7,28 +7,9 @@ public class TestDog {
 		Dog d2 = new Dog();
 		Dog d3 = new Dog();
 		
-		d1.breed = "Bulldog";
-		d1.size = "Large";
-		d1.colour ="Laight gray";
-		d1.age = 5;
-		
-		d2.breed = "Beagle";
-		d2.size = "Large";
-		d2.colour ="Orange";
-		d2.age = 6;
-		
-		d3.breed = "German shepherd";
-		d3.size = "Large";
-		d3.colour ="White and orange";
-		d3.age = 6;
-		
-		d1.Doginfo();
-		d2.Doginfo();
-		d3.Doginfo();
-		
-		
-		
-		
+		d1.Doginfo("Bulldog" , "Large" , "Laight gray" , 5);
+		d2.Doginfo("Beagle" , "Large" , "Orange" , 6);
+		d3.Doginfo("German shepherd" , "Large" , "White and orange" , 6);
+	
 	}
-
 }
